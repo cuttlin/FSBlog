c00d755ceb305dca39e2e0807f4f55855ee2588c
cuttlin <847612460@qq.com>
2019-09-28 22:10:01 +0800

    fix not find .css/img bug

--- FSBlog/Controllers/BlogController.cs
+++ FSBlog/Controllers/BlogController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using NuGet.Frameworks;
 namespace FSBlog.Controllers
 {
     public class BlogController : Controller
