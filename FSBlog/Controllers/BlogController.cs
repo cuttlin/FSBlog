c5e6a3eaa0d137e05a0ee90ace841d306ffc332c
cuttlin <847612460@qq.com>
2019-09-28 21:17:18 +0800

    update index style

---
+++ FSBlog/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Frameworks;
+namespace FSBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult BlogList()
+        {
+            return View();
+        }
+    }
+}
